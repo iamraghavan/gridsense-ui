89111eb62cf34177ab3ca01375b7a181bb58c0a1
Ramanan Raju <ramanan48@gmail.com>
2025-08-06 13:30:31 +0000

    Failed to fetch user: "Not Found"
    src/lib/auth.ts (22:15) @ getUser

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,4 +1,5 @@
 
-export const API_URL = 'https://node-sensor-gird.onrender.com/api';
+export const API_URL = 'https://node-sensor-grid.onrender.com/api';
 export const API_KEY = 'a0ea2188-ee2f-46d2-9661-310bed43c3bf';
 export const AUTH_TOKEN_COOKIE_NAME = 'auth_token';
+
