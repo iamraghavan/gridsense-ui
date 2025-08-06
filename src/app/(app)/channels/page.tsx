05fe1e273dbdc31ad5f49109ec47336181b387af
Ramanan Raju <ramanan48@gmail.com>
2025-08-06 16:30:30 +0000

    Console Error
    Hydration failed because the server rendered HTML didn't

--- src/app/(app)/channels/page.tsx
+++ src/app/(app)/channels/page.tsx
@@ -256,7 +256,9 @@ export default function ChannelsPage() {
   };
 
   useEffect(() => {
-    fetchChannels();
+    if (token) {
+        fetchChannels();
+    }
   }, [token]);
 
   return (
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
 
-
 import type {Metadata} from 'next';
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster";
@@ -21,12 +20,10 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Source+Code+Pro&display=swap" rel="stylesheet" />
       </head>
-      <body className="font-body antialiased">
+      <body className="font-body antialiased" suppressHydrationWarning>
         {children}
         <Toaster />
       </body>
     </html>
   );
 }
-
-    
