f6515df51aeddf648ac01131120ea183f07cc2fa
Ramanan Raju <ramanan48@gmail.com>
2025-08-09 07:01:22 +0000

    EI duplicate key error collection: test-users index usemame_l dup key: {

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -59,8 +59,8 @@ export default function RegisterPage() {
       <CardContent>
         <form action={formAction} className="space-y-4">
             <div className="space-y-2">
-                <Label htmlFor="username">Full Name</Label>
-                <Input id="username" name="username" placeholder="John Doe" required />
+                <Label htmlFor="name">Full Name</Label>
+                <Input id="name" name="name" placeholder="John Doe" required />
             </div>
              <div className="space-y-2">
                 <Label htmlFor="email">Email</Label>
